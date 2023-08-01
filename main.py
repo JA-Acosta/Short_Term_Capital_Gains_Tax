77c81c909c77ddebb011ccb21bb9c5a0a06eea5c
JAAR <joe.aaron.acosta@outlook.com>
2023-08-01 19:17:55 -0400

    Version 1

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+JAAR
+08/01/2023
+Short Term Gain Tax
+Version 1
+'''
+
+'''
+Generates a program that calculates the short-term capital gains tax based on an initial amount, terms in days, and a daily interest rate.
+'''
+
+def main() :
+
+
+if __name__ == '__main__' :
+    main()
